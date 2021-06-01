b46dae1c56ab63a545f737641cf5f12d758a0f7e
jdramsey <jdramsey@andrew.cmu.edu>
2021-06-01 18:11:11 -0400

    BOSS and K2

--- tetrad-lib/src/main/java/edu/cmu/tetrad/search/BestOrderScoreSearch.java
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/search/BestOrderScoreSearch.java
@@ -40,65 +40,17 @@ public class BestOrderScoreSearch {
      */
     public Graph search(List<Node> variables) {
         List<Node> b0 = new ArrayList<>(variables);
-        Graph g0 = fastForward.search(b0);
-        double s0 = fastForward.score();
+//        Graph g0 = fastForward.search(b0);
+        double s0 = fastForward.score(b0);
 
         scoreOriginalOrder = s0;
 
         System.out.println("Original order = " + b0);
 
-        Set<List<Node>> tabu = new HashSet<>();
-
         boolean changed = true;
 
         int count = 0;
 
-//        while (++count < 100000) {
-//            if (RandomUtil.getInstance().nextDouble() < 0.01) {
-//                b0 = new ArrayList<>(variables);
-//                shuffle(b0);
-//            }
-//
-//            changed = false;
-//
-//            for (int i = 0; i < b0.size() - 1; i++) {
-//                int j = i + 1;
-////                for (int j = 0; j < b0.size(); j++) {
-//                List<Node> b1 = new ArrayList<>(b0);
-//
-//                Node n1 = b1.get(i);
-//                Node n2 = b1.get(j);
-//
-//                b1.remove(n1);
-//                b1.add(j, n1);
-//
-//                b1.remove(n2);
-//                b1.add(i, n2);
-//
-//                if (tabu.contains(b1)) continue;
-//                tabu.add(b1);
-//
-//                Graph g1 = fastForward.search(b1);
-//                double s1 = fastForward.score();
-//
-//                if (s1 < s0) {
-//                    s0 = s1;
-//                    g0 = g1;
-//                    changed = true;
-//                }
-//            }
-////            }
-//        }
-//
-//        int count = 0;
-//
-//        boolean changed = true
-
-//        List<Node> b2 = b0;
-//        Graph g2 = g0;
-//        double s2 = s0;
-//
-//        WHILE:
         while (changed) {
             changed = false;
 
@@ -110,32 +62,23 @@ public class BestOrderScoreSearch {
 
                     count++;
 
-                    Graph g1 = fastForward.search(b1);
-                    double s1 = fastForward.score();
+                    double s1 = fastForward.score(b1);
 
                     if (s1 < s0) {
-
-                        if (tabu.contains(b1)) continue;
-                        tabu.add(b1);
-
                         s0 = s1;
                         b0 = b1;
-                        g0 = g1;
+//                        g0 = g1;
                         changed = true;
                     }
                 }
             }
-
-//            b2 = b0;
-//            g2 = g0;
-//            s2 = s0;
 
             System.out.println("Updated order = " + b0 + " count = " + count);
         }
 
         scoreLearnedOrder = s0;
 
-        return g0;
+        return fastForward.search(b0);
     }
 
     public double getScoreOriginalOrder() {
