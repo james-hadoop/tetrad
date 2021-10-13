d4755439eb68d81b9abb003ab19171f95bcbcfad
Joseph Ramsey <jdramsey@andrew.cmu.edu>
2021-10-13 11:35:39 -0400

    Committing moving knowledge editor to data, tightly coupling knowledge to data.

---
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/session/AvailableModels.java
@@ -0,0 +1,29 @@
+package edu.cmu.tetrad.session;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AvailableModels {
+    private static final AvailableModels unique = new AvailableModels();
+    private final Set<Object> set = new HashSet<>();
+
+    public static AvailableModels getUnique() {
+        return unique;
+    }
+
+    public Set<Object> getSet() {
+        return new HashSet<>(set);
+    }
+
+    public void add(Object model) {
+        if (model == null) return;
+        set.add(model);
+        System.out.println("Added " + set);
+    }
+
+    public void remove(Object model) {
+        if (model == null) return;
+        set.remove(model);
+        System.out.println("Removed " + set);
+    }
+}
